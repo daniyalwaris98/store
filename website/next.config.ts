07833fc27e5252c700d42e4093c82f3ea9cc5cf5
daniyalwaris98 <daniyal@vyadom.com>
2026-06-29 21:23:14 +0500

    removed errors

--- website/next.config.ts
+++ website/next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "s3.robodialog.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
